e4b65c4a0e1e8b340ef7e1a685135d20ab697f78
lethuandat <98041731+lethuandat@users.noreply.github.com>
2022-06-13 13:53:17 +0700

    Update quan-ly-sinh-vien.sql

--- ss01_csdl_quan_he/bai_tap/quan_ly_sinh_vien/quan-ly-sinh-vien.sql
+++ ss01_csdl_quan_he/bai_tap/quan_ly_sinh_vien/quan-ly-sinh-vien.sql
@@ -1,17 +1,17 @@
 create database `student_management`;
 use `student_management`;
 create table `student` (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(45),
     age int,
     country varchar(45)
 );
 create table `class` (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(45)
 );
 create table `teacher` (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(45),
     age int,
     country varchar(45)
