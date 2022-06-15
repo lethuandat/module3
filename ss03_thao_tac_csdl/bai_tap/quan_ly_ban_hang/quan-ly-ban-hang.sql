7b6365a9faf965839a7be2abb4cf866123608931
lethuandat <levanle5@gmail.com>
2022-06-15 15:41:01 +0700

    update bai tap

--- ss03_thao_tac_csdl/bai_tap/quan_ly_ban_hang/quan-ly-ban-hang.sql
+++ ss03_thao_tac_csdl/bai_tap/quan_ly_ban_hang/quan-ly-ban-hang.sql
@@ -39,11 +39,8 @@ insert into order_detail
 values(2, 3, 3);
 
 -- Hiển thị các thông tin gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
-select o.o_id, o.o_date, sum(p.p_price * od.od_quantity) as tong_tien_hoa_don
-from product p
-join order_detail od on od.p_id = p.p_id
-join `order` o on o.o_id = od.o_id
-group by o.o_id;
+select o_id, o_date, o_total_price
+from  `order`;
 
 -- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
 select c.c_name, p.p_name
@@ -60,8 +57,8 @@ group by o.o_id
 having so_lan_mua = 0;
 
 -- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
-select o.o_id, o.o_date, (od.od_quantity * p.p_price) as total_price
-from `order` o
+select o.o_id, o.o_date, sum(p.p_price * od.od_quantity) as o_price
+from  `order` o
 join order_detail od on od.o_id = o.o_id
 join product p on p.p_id = od.p_id
 group by o.o_id;
